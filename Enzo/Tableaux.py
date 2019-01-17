22b7420211444a2cf8577753c98f7953f249d433
Unknown <enz.dard@gmail.com>
2019-01-17 19:01:15 +0100

    [FINI] Définition des tableaux

--- Enzo/Tableaux.py
+++ Enzo/Tableaux.py
@@ -1,18 +1,27 @@
-#Définition du tableau principal
-tables = [0, 1, 2,\
-          3, 4, 5,\
-          6, 7, 8] #Tableaux de 9 sous-tableaux
-
 #Définition des tableaux secondaires
 #Tableaux secondaires si par exemple tableSecond[0] == 0 personne n'a joué sur cette case
 #                                    tableSecond[0] == 1 le joueur 1 a joué sur cette case
 #                                    tableSecond[0] == 2 le joueur 2 a joué sur cette case
-def createAllSecondariesTables():
+def createTables():
+    """
+    Cette fonction crée le tableau principal.
+    Le tableau principal contient les 9 tableaux secondaires.
+    La fonction renvoit le tableau principal.
+    """
+    #Définition du tableau principal
+    tables = [0, 1, 2,\
+              3, 4, 5,\
+              6, 7, 8] #Tableaux de 9 sous-tableaux
+
+    #Définition des 9 tableaux secondaires
     for i in range(9):
         tableSecond = [0, 0, 0,\
                        0, 0, 0,\
                        0, 0, 0]
         tables[i] = tableSecond
 
-createAllSecondariesTables()
-print(tables)
+    #Renvoit du tableau principal contenant les 9 tableaux secondaires
+    return tables
+
+#EXEMPLE d'utilisation de la fonction:
+#   tableauPrincipal = Tableaux.createTables()
